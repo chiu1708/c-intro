1afa9de44f51f6f0786e19c9ea73c79e2411dad0
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-04-09 15:56:40 +0700

    Update baitest1.c

--- baitest1.c
+++ baitest1.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main()
+void ktcd(int a, int b) {printf("%lf va %lf %s dau!", a, b, a*b > 0)  ? "cung" : "khong cung");}
+
+void main()
 {
     double a, b;
     printf("nhap mot so: "); scanf("%lf", &a);
     printf("nhap them mot so nua: "); scanf("%lf", &b);
     
-    printf("%lf va %lf %s dau!", a, b, a*b > 0)  ? "cung" : "khong cung");
 }
-
-
