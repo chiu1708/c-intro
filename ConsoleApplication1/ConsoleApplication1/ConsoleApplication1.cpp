9ceb7dc17a4517e515e228ebdf871f53532161ef
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-04-16 17:01:59 +0700

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,6 @@
 
 #include <stdio.h>
 
-int g(int n) {
-	return n <= 1 ? n : n*g(n-1);
-}
-
 void r(int n) {
 	if (n == 0) { return; }
 	//print truoc lap lai sau.
@@ -18,10 +14,4 @@ int b(int n) {
 	//lap lai truoc print sau
 	b(n/2);
 	printf("%d", n % 2);
-}
-
-void main() {
-	int n;
-	printf("Nhap so ban muon dao: "); scanf_s("%d", &n);
-	printf("%d dao lai la: %d", n); r(n);
 }
