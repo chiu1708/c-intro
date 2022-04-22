c76e1b803b45bfe488860dcfcd563d7a4186139c
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-04-22 22:18:38 +0700

    Add files via upload

---
+++ NhapVaoVaDemChuSo.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int Dem(x) {
+    return x / 10 == 0 ? 1 : 1 + Dem(x / 10);
+}
+
+void EnterAndCount() {
+    //Nhap vao mot day so
+    int Day[4], i = 1;
+    for (int i = 0; i < 4; i++) {
+        printf("value[%d] = ", i); scanf_s("%d", &Day[i]);
+        
+    }
+
+    //Dem so chu so
+    for (int i = 0; i < 4; i++) {
+        printf("So %d co chieu dai la %d.\n", Day[i], Dem(Day[i]));
+    }
+
+    return;
+}
+
+void main()
+{
+    EnterAndCount();
+}
