e2b34bfb7656937c15f60f08ac42782a375b09a0
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-04-22 17:12:20 +0700

    Add files via upload

---
+++ NhapVaoVaLoaiRaPhanTuTrung.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+
+void EnterAndSort() {
+    //Nhap vao mot day so
+    int Day[12], BienTam;
+    for (int i = 0; i < 12; i++) {
+        printf("value[%d] =", i); scanf_s("%d", &Day[i]);
+    }
+
+    //Xuat ra man hinh day so
+    printf("Day so ban da nhap vao:");
+    for (int i = 0; i < 12; i++) {
+        printf(" %d", Day[i]);
+    }
+    printf("\n");
+
+    //Sắp xếp từ lớn tới nhỏ
+    for (int i = 12; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (Day[j] > Day[j + 1]) {
+                BienTam = Day[j];
+                Day[j] = Day[j + 1];
+                Day[j + 1] = BienTam;
+            }
+        }
+    }
+
+    //Xuat day da loai cac phan tu trung ra man hinh
+    printf("Day so da sap xep tang dan: ");
+    for (int i = 0; i < 12; i++) {
+        if (Day[i] != Day[i - 1]) {
+            printf("%d ", Day[i]);
+        }
+    }
+
+    return;
+}
+
+void main()
+{
+    EnterAndSort();
+}
