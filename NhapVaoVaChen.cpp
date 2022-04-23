7cf8804209ea62362bae81aa126d9c6fbb2282ad
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-04-23 17:22:41 +0700

    Update NhapVaoVaChen.cpp

--- NhapVaoVaChen.cpp
+++ NhapVaoVaChen.cpp
@@ -1,43 +1,41 @@
+//Da sua bai
+//
+
 #include <stdio.h>
 
-void EnterAndInsert() {
-    //Nhap vao mot day so
-    int Day[12],DayPhu[1000], BienTam, SoCanChen, CanGan, ViTri, DoDai = 12;
-    for (int i = 0; i < 12; i++) {
-        printf("value[%d] = ", i); scanf_s("%d", &Day[i]);
-        DayPhu[i] = Day[i];
-    }
+void EnterAndCount() {
+	//Nhap vao mot day so
+	int n, m, a[1000], item[1000], position[1000];
+	printf("Nhap n = "); scanf("%d", &n);
+	for (int i = 0; i < 12; i++) {
+		printf("value[%d] = ", i); scanf("%d", &a[i]);
+	}
 
-    //Chen cac phan tu vao day
-    printf("Ban muon chen them bao nhieu phan tu: "); scanf_s("%d", &SoCanChen);
-    for (int i = 1; i <= SoCanChen; i++) {
-        printf("Phan tu thu %d ban muon nhap la: ", i); scanf_s("%d", &BienTam);
-        printf("Vi tri can chen phan tu nay la: "); scanf_s("%d", &ViTri);
-        DoDai++;
-        for (int j = ViTri; j < DoDai; j++) {
-            CanGan = BienTam;
-            BienTam = DayPhu[j];
-            DayPhu[j] = CanGan;
+	//Them cac phan tu 
+    printf("Ban muon them bao nhieu phan tu: "); scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        printf("item[%d] = ", i+1); scanf("%d", &item[i]);
+        printf("tai vi tri: "); scanf("%d", &position[i]);
+    }
+    
+    //Chen
+    for (int i = 0; i < m; i++) {
+        int item = items[i];
+        int position = position[i];
+        int j;
+        for(j = n++; j > position; j--) {
+            a[j] = a[j-1];
         }
+        a[j] = item;
     }
-
-    //Xuat ra man hinh day so
-    printf("Day so ban da nhap vao:");
-    for (int i = 0; i < 12; i++) {
-        printf(" %d", Day[i]);
-    }
-    printf("\n");
-
-    //Xuat day so da chen
-    printf("Day so da chen them cac phan tu vao la: ");
-    for (int i = 0; i < DoDai; i++) {
-        printf("%d ", DayPhu[i]);
-    }
-
+    
+    //In ra
+    
+    
     return;
 }
 
 void main()
 {
-    EnterAndInsert();
+	EnterAndCount();
 }
