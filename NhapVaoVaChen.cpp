5613df37bef7446062403131a71f5e207d29c3e4
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-04-23 09:56:54 +0700

    Update NhapVaoVaChen.cpp

--- NhapVaoVaChen.cpp
+++ NhapVaoVaChen.cpp
@@ -1,23 +1,26 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
-void EnterAndSort() {
+void EnterAndInsert() {
     //Nhap vao mot day so
-    int Day[12], BienTam, CanGan, ViTri, SoCanGan, DoDai;
+    int Day[12],DayPhu[24], BienTam, SoCanChen, CanGan, ViTri, DoDai = 12;
     for (int i = 0; i < 12; i++) {
         printf("value[%d] =", i); scanf_s("%d", &Day[i]);
+        DayPhu[i] = Day[i];
     }
-
-    //Them phan tu vao day phu
-    printf("Ban muon chen them bao nhieu phan tu: "); scanf_s("%d", &SoCanGan);
-    DoDai = 12 + SoCanGan;
-    int DayPhu[DoDai];
 
     //Chen cac phan tu vao day
-    for (int i = 1; i <= SoCanGan; i++) {
+    printf("Ban muon chen them bao nhieu phan tu: "); scanf_s("%d", &SoCanChen);
+    for (int i = 1; i <= SoCanChen; i++) {
         printf("Phan tu thu %d ban muon nhap la: ", i); scanf_s("%d", &BienTam);
         printf("Vi tri can chen phan tu nay la: "); scanf_s("%d", &ViTri);
+        DoDai++;
+        for (int j = ViTri; j < DoDai; j++) {
+            CanGan = BienTam;
+            BienTam = DayPhu[j];
+            DayPhu[j] = CanGan;
+        }
     }
-    
+
     //Xuat ra man hinh day so
     printf("Day so ban da nhap vao:");
     for (int i = 0; i < 12; i++) {
@@ -27,11 +30,14 @@ void EnterAndSort() {
 
     //Xuat day so da chen
     printf("Day so da chen them cac phan tu vao la: ");
+    for (int i = 0; i < DoDai; i++) {
+        printf("%d ", DayPhu[i]);
+    }
 
     return;
 }
 
 void main()
 {
-    EnterAndSort();
+    EnterAndInsert();
 }
