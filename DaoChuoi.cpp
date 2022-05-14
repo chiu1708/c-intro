b7027805e75b1719a8614ba0344f44294302657e
chiu1708 <101632726+chiu1708@users.noreply.github.com>
2022-05-14 07:31:11 +0700

    Add files via upload

---
+++ DaoChuoi.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int strlen(char str[]) {
+	int i = 0;
+	while (str[i] != '\0') { i++; }
+	return i;
+}
+
+void DaoChuoi(char str[]) {
+	char BienTam = str[strlen(str) - 1], CanGan;
+	for (int i = 0; i < strlen(str); i++) {
+		CanGan = BienTam; BienTam = str[i]; str[i] = CanGan;
+	}
+}
+
+void main() {
+	char str[1000];
+	printf("Nhap vao mot chuoi: "); fgets(str, sizeof(str), stdin);
+	DaoChuoi(str);
+	printf("Ket qua: %s", str);
+}
